670a8c127030488c48fe473bf36b9da47fe7056a
Jacobs066 <isrealabaefah2005@gmail.com>
2024-07-01 12:13:38 +0000

    zparcare

--- main.cpp
+++ main.cpp
@@ -3,34 +3,34 @@
 using namespace std;
 
 struct Employee {
-    string name;
-    double hourlyRate;
-    double hoursWorked;
+  string name;
+  double hourlyRate;
+  double hoursWorked;
 };
 
 int main() {
-    const int NUM_EMPLOYEES = 1;
-    Employee employees[NUM_EMPLOYEES];
+  const int NUM_EMPLOYEES = 1;
+  Employee employees[NUM_EMPLOYEES];
 
-    // Get employee information
-    for (int i = 0; i < NUM_EMPLOYEES; i++) {
-        cout << "Enter employee " << i+1 << " name: ";
-        getline(cin, employees[i].name);
+  // Get employee information
+  for (int i = 0; i < NUM_EMPLOYEES; i++) {
+    cout << "Enter employee " << i + 1 << " name: ";
+    getline(cin, employees[i].name);
 
-        cout << "Enter hourly rate for " << employees[i].name << ": ";
-        cin >> employees[i].hourlyRate;
+    cout << "Enter hourly rate for " << employees[i].name << ": ";
+    cin >> employees[i].hourlyRate;
 
-        cout << "Enter hours worked by " << employees[i].name << ": ";
-        cin >> employees[i].hoursWorked;
-        cin.ignore(); // Clear newline character
-    }
+    cout << "Enter hours worked by " << employees[i].name << ": ";
+    cin >> employees[i].hoursWorked;
+    cin.ignore(); // Clear newline character
+  }
 
-    // Calculate and display payroll
-    cout << "\nPayroll:" << endl;
-    for (int i = 0; i < NUM_EMPLOYEES; i++) {
-        double grossPay = employees[i].hourlyRate * employees[i].hoursWorked;
-        cout << employees[i].name << " - $" << grossPay << endl;
-    }
+  // Calculate and display payroll
+  cout << "\nPay cafe roll:" << endl;
+  for (int i = 0; i < NUM_EMPLOYEES; i++) {
+    double grossPay = employees[i].hourlyRate * employees[i].hoursWorked;
+    cout << employees[i].name << " - $" << grossPay << endl;
+  }
 
-    return 0;
+  return 0;
 }
